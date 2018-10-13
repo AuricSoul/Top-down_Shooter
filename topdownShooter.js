3f69cb169e3ccd8c9dfe55096ec0181dbe991809
SuperCool25 <38873365+SuperCool25@users.noreply.github.com>
2018-10-12 19:31:49 -0600

    Update topdownShooter.js

--- topdownShooter.js
+++ topdownShooter.js
@@ -16,12 +16,28 @@ var entitys;
 
 function init() {
   
+    
+    
+    gameloop();
 }
 function render(){
 
 }
 //dt is delta time
 functino update(dt){}
+
+var timer = new Date();
+var lastTime;
+
 function gameloop(){
-  
+    lastTime = timer.getTime();
+    while(running){
+        var time = timer.getTime();
+        var delta = time - timer.lastTime;
+        lastTime = time;
+        
+        update(delta);
+        render();
+    
+    }
 } 
