230be97f4ba893d14678ad7c4d7540808b4678b2
SuperCool25 <38873365+SuperCool25@users.noreply.github.com>
2018-10-12 19:25:39 -0600

    Update topdownShooter.js

--- topdownShooter.js
+++ topdownShooter.js
@@ -1,3 +1,27 @@
+
+var running = true;
+
+class sprite{
+    
+    constructor( x, y){
+      this.x = x;
+      this.y = y;
+    },
+  
+    update: function(deltaTime){},
+    render: function(){}
+}
+
+var entitys;
+
 function init() {
   
 }
+function render(){
+
+}
+//dt is delta time
+functino update(dt){}
+function gameloop(){
+  
+} 
