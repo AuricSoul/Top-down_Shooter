9d3ac89a8f253db4022222b665b6ce48907464c8
SuperCool25 <38873365+SuperCool25@users.noreply.github.com>
2018-10-12 19:44:45 -0600

    Update topdownShooter.js

--- topdownShooter.js
+++ topdownShooter.js
@@ -1,30 +1,46 @@
 
+
+var gameCanvas = document.getElementById("GameWindow");
+var context = gameCanvas.getContext("2d");
+
+
 var running = true;
 
 class sprite{
     
-    constructor( x, y){
+    constructor( x, y, imagePath){
       this.x = x;
       this.y = y;
+      this.image = new Image();
+      this.image.src = imagePath;
     },
   
     update: function(deltaTime){},
-    render: function(){}
+    render: function(){
+        context.drawImage(this.image, this.x , this.y);
+    }
 }
 
-var entitys;
+var entitys = new Array();
 
 function init() {
   
-    
+    entitys[0] = new sprite(250,250, "res/cow.png");
     
     gameloop();
 }
 function render(){
-
+    
+    for(var i = 0; i < entitys.length; i++){
+        entitys[i].render();
+    }
 }
 //dt is delta time
-functino update(dt){}
+functino update(dt){
+    for(var i = 0; i < entitys.length; i++){
+        entitys[i].update(dt);
+    }
+}
 
 var timer = new Date();
 var lastTime;
